8a99cdcc5e335cd1786be24036e35657f394e65b
Igor Fabiano Nazar <nazar@objective.com.br>
2017-07-22 21:49:30 -0300

    Templates

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ['Java', 'Ex JS', 'Angular'];
+
+  constructor() { 
+    this.nomePortal = 'http://loaine.training';
+  }
 
   ngOnInit() {
   }
