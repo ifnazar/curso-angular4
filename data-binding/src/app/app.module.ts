8b6811a265f9c0aac7c644522aaad554d333f0ca
Igor Fabiano Nazar <nazar@objective.com.br>
2017-07-25 17:58:48 -0300

    remove directory "data-binding"

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -3,19 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
-import { DataBindingClassStyleComponent } from './data-binding/data-binding-class-style/data-binding-class-style.component';
-import { DataBindingInterpolationComponent } from './data-binding/data-binding-interpolation/data-binding-interpolation.component';
-import { DataBindingEventComponent } from './data-binding/data-binding-event/data-binding-event.component';
-import { DataBindingTwoWayBindingComponent } from './data-binding/data-binding-two-way-binding/data-binding-two-way-binding.component';
-import { DataBindingIoPropertiesComponent } from './data-binding/data-binding-io-properties/data-binding-io-properties.component';
-import { InputPropertiesComponent } from './data-binding/input-properties/input-properties.component';
-import { OutputPropertiesComponent } from './data-binding/output-properties/output-properties.component';
+import { DataBindingClassStyleComponent } from './data-binding-class-style/data-binding-class-style.component';
+import { DataBindingInterpolationComponent } from './data-binding-interpolation/data-binding-interpolation.component';
+import { DataBindingEventComponent } from './data-binding-event/data-binding-event.component';
+import { DataBindingTwoWayBindingComponent } from './data-binding-two-way-binding/data-binding-two-way-binding.component';
+import { DataBindingIoPropertiesComponent } from './data-binding-io-properties/data-binding-io-properties.component';
+import { InputPropertiesComponent } from './input-properties/input-properties.component';
+import { OutputPropertiesComponent } from './output-properties/output-properties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent,
     DataBindingClassStyleComponent,
     DataBindingInterpolationComponent,
     DataBindingEventComponent,
